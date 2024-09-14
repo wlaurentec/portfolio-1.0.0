ec393fae232aba55125ac8dcbc369e7e3674506e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-14 08:15:44 -0500

    add Services component structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
+import MyWork from "./components/MyWork.jsx/MyWork";
 import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services.jsx/Services";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Hero />
       <About />
+      <Services /> */}
+      <MyWork />
     </div>
   );
 };
