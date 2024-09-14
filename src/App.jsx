b97624b47ca6985a25a4d60f614641ea30f80cc0
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-13 20:49:57 -0500

    add about component jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
---
+++ src/components/About/About.jsx
@@ -0,0 +1,66 @@
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_profile.svg";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am an experienced Fronted Developer with over a decade of
+              professional experience in the field. Throughout my career, I have
+              had the previlege of colllaborating with prestigious
+              organizations, contributing to their success and growth.
+            </p>
+            <p>
+              My passion for fronted development is not only reflected in my
+              extensive experience but also in the enthusiasm and dedication I
+              bring to each project.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p> <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p> <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>javaScript</p> <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next JS</p> <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
